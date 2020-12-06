c24ab8503cd54a98039368f7a5fb3dd145c32a10
Erik Sigurd Young <sigurd@lambda-conspiracy.net>
2020-12-06 09:08:30 -0500

    reorg

---
+++ day01/puzzle-02.sh
@@ -0,0 +1,18 @@
+#! /bin/zsh
+
+input_numbers=$1
+
+for m in $(cat $input_numbers) ; do
+    for n in $(cat $input_numbers | grep -v "^$m$") ; do
+	o=$((2020-$m-$n))
+	if (( o > 0 )) ; then
+	   if grep -o "^$o$" input.txt ; then
+	       echo answer is $m and $n and $o = $(($m*$n*$o))
+	   fi
+	fi
+    done
+    
+done | grep answer | head -1
+
+### xyzzy ###
+
