a13244254a303971daaf720320cedda69601a57c
Erik Sigurd Young <sigurd@lambda-conspiracy.net>
2020-12-08 14:26:36 -0500

    day 4 is broken?

--- day04/puzzle-01.sh
+++ day04/puzzle-01.sh
@@ -2,32 +2,22 @@
 
 input_file=$1
 
-typeset number_of_trees=0
-typeset forest_lines=$(wc -l $input_file | awk '{print $1}')
-typeset tree_slots=$(($(head -1 input.txt | awk '{print length}')))
-typeset hpos=1
-typeset hjump=3
+typeset valid_entries=0
 
-for (( i=2 ; i<=$forest_lines ; i++ )) ; do
-    typeset cline=$(sed -n ${i}p $input_file)
-    ((hpos+=$hjump))
+awk -v RS= '{$1=$1}1' $input_file | while read m ; do
+    typeset m_fields=$(($(echo $m | awk '{print NF}')))
 
-    if (( $hpos > $tree_slots )) ; then
-	typeset cpos=$(($hpos%$tree_slots))
-	if (( cpos == 0 )) ; then
-	    cpos=$tree_slots
-	fi
-    else
-	typeset cpos=$hpos
-    fi
-
-    typeset cobj=$(echo $cline | grep -o . | grep -n . | sed -n ${cpos}p | awk -F: '{print $2}')
-    
-    if [[ $cobj = \# ]] ; then
-	((number_of_trees++))
-    fi
+    case $m_fields in
+	8)  ((valid_entries+=1))
+	    ;;
+	7)  if ! echo $m | grep -q 'cid:' ; then
+		((valid_entries+=1))
+	    fi
+	    ;;
+	*)  ;;
+    esac
 done
 
-echo There are $number_of_trees trees.
+echo There are $valid_entries entries.
 
 ### xyzzy ###
