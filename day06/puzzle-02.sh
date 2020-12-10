87a30f9234686825774ce477b9fcdab5d41de158
Erik Sigurd Young <sigurd@lambda-conspiracy.net>
2020-12-09 20:15:33 -0500

    fixed a ridiculous keyboard-o

--- day06/puzzle-02.sh
+++ day06/puzzle-02.sh
@@ -7,7 +7,7 @@ typeset count=0
 cat $input_file | awk -v RS= -v ORS=' \n' '{$1=$1}1' | while read m ; do
     typeset people=$(echo $m | awk '{print NF}')
     
-    typeset common_answerss=$(echo $m | grep -o . | grep '[a-z]' | sort | uniq -c | grep '['$people']' | wc -l)
+    typeset common_answers=$(echo $m | grep -o . | grep '[a-z]' | sort | uniq -c | grep '['$people']' | wc -l)
     ((count+=$common_answers))
 done
 
