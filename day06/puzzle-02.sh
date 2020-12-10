7ada2ad395893a69f0239fa6dde7b06124ec2292
Erik Sigurd Young <sigurd@lambda-conspiracy.net>
2020-12-09 19:56:36 -0500

    solved day 6, onto day 5

---
+++ day06/puzzle-02.sh
@@ -0,0 +1,16 @@
+#! /bin/zsh
+
+typeset input_file=$1
+
+typeset count=0
+
+cat $input_file | awk -v RS= -v ORS=' \n' '{$1=$1}1' | while read m ; do
+    typeset people=$(echo $m | awk '{print NF}')
+    
+    typeset common_answerss=$(echo $m | grep -o . | grep '[a-z]' | sort | uniq -c | grep '['$people']' | wc -l)
+    ((count+=$common_answers))
+done
+
+echo $count
+
+### xyzzy ###
